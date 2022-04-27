7b244325035d8310812b6b43ac727f10302cc9e6
Davi Alexandrias de Oliveira <dalexandrias@hotmail.com>
2022-04-27 18:05:44 -0300

    Adionado status http e hash nas senhas

--- backend/susej/services/auth_services.py
+++ backend/susej/services/auth_services.py
@@ -1,6 +1,6 @@
 from susej.constants.status_return import CREATED_USER, EMAIL_INVALID, ERROR, SUCCESS, EMAIL_ALREADY_EXISTS
-from typing import Any
 from flask import jsonify
+from http import HTTPStatus
 from werkzeug.security import check_password_hash, generate_password_hash
 import validators
 from susej.commands_dml.commands import insert_table
@@ -12,28 +12,41 @@ class AuthService():
     def __init__(self) -> None:
         pass
     
-    def save_new_user(new_user: User) -> jsonify:
+    def new_user(new_user: User) -> jsonify:
         user = new_user.query.filter_by(email=new_user.email).first()
         if not user:
             if validators.email(new_user.email):
+                
+                new_user.password = generate_password_hash(new_user.password)
+                
                 insert_table(new_user)
-                return jsonify(
+                
+                response = jsonify(
                     {
                         "status": SUCCESS,
                         "mensage": CREATED_USER
                     }
-                ), 201
+                )
+                response.status_code = HTTPStatus.CREATED
+                
+                return response
             else:
-                return jsonify(
+                response = jsonify(
                     {
                         "status": ERROR,
                         "mensage": EMAIL_INVALID
                     }
                 )
+                response.status_code = HTTPStatus.BAD_REQUEST
+                
+                return response
         else:
-            return jsonify(
+            response = jsonify(
                 {
                     "status": ERROR,
                     "mensage": EMAIL_ALREADY_EXISTS
                 }
             )
+            response.status_code = HTTPStatus.BAD_REQUEST
+            
+            return response
