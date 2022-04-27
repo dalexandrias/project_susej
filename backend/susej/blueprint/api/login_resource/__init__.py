8209fe53af52b71ead4454f7d50a20baced62722
Davi Alexandrias de Oliveira <dalexandrias@hotmail.com>
2022-04-26 22:42:54 -0300

    Criado a rota de login

--- backend/susej/blueprint/api/login_resource/__init__.py
+++ backend/susej/blueprint/api/login_resource/__init__.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, Flask
 from flask_restful import Api
-from .resource import CreateUser
+from .auth import CreateUser
 
 bp = Blueprint('login', __name__, url_prefix='/api')
 api = Api(bp)
--- backend/susej/model/login.py
+++ backend/susej/model/login.py
@@ -1,8 +1,11 @@
 import datetime
+from email.policy import default
+from enum import unique
 
 from susej.extension.database import db
 from sqlalchemy_serializer import SerializerMixin
 from flask_migrate import Migrate
+from datetime import datetime
 
 
 migrate = Migrate()
@@ -11,18 +14,17 @@ class User(db.Model, SerializerMixin):
     __tablename__ = 'USERS'
     
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(255), nullable=False)
-    password = db.Column(db.String(255), nullable=False)
-    creation_date = db.Column(db.DateTime, nullable=False)
+    email = db.Column(db.String(255), nullable=False, unique=True)
+    password = db.Column(db.Text(), nullable=False)
+    creation_date = db.Column(db.DateTime, nullable=False, default=datetime.now())
     
-    def __init__(self, email: str, password: str, creation_date: datetime.datetime) -> None:
+    def __init__(self, email: str, password: str) -> None:
         self.email = email
         self.password = password
-        self.creation_date = creation_date
         
     def __repr__(self) -> str:
         return f"Login {self.email}"
-    
+
 
 def init_app(app):
     migrate.init_app(app, db)
