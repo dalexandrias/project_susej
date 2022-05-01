9da9d9ffc4ee958365a2ebffa475f82b14c4ab1b
Davi Alexandrias de Oliveira <dalexandrias@hotmail.com>
2022-05-01 18:24:31 -0300

    Criado service get all e rota get

--- backend/susej/services/client_service.py
+++ backend/susej/services/client_service.py
@@ -1,5 +1,5 @@
 import json
-from susej.commands_dml.commands import insert_table
+from susej.commands_dml.commands import insert_table, select_table
 from susej.constants.status_return import CREATED_CLIENT, SUCCESS
 from susej.model.client_model import Client
 
@@ -8,10 +8,13 @@ class ClientService(object):
     def __init__(self) -> None:
         pass
 
-    def client_save(self, client_in: Client) -> json:
+    def save_client(self, client_in: Client) -> json:
         insert_table(client_in)
 
         return {
             "status": SUCCESS,
             "message": CREATED_CLIENT
         }
+        
+    def get_all_client(self) -> json:
+        return select_table(Client)
