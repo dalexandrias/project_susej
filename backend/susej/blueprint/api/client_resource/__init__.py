69ef0a4cd280a5b6a1fa045dd761991602f51f71
Davi Alexandrias <77761467+dalexandrias@users.noreply.github.com>
2022-08-29 23:52:44 -0300

    Delete backend directory

--- backend/susej/blueprint/api/client_resource/__init__.py
+++
@@ -1,6 +0,0 @@
-from flask import Flask
-from susej.blueprint.api.client_resource.client_controller import bp
-
-
-def init_app(app: Flask) -> None:
-    app.register_blueprint(bp)
--- backend/susej/blueprint/api/login_resource/__init__.py
+++
@@ -1,6 +0,0 @@
-from flask import Flask
-from susej.blueprint.api.login_resource.auth_controller import bp
-
-
-def init_app(app: Flask):
-    app.register_blueprint(bp)
